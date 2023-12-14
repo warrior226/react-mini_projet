d28eab2455c50bf3ba3948c222516265a9d8f487
Pato <75866080+warrior226@users.noreply.github.com>
2023-12-14 22:43:42 +0000

    mini projet d'une calculatrice en react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [displayValue, setDisplayValue] = useState("");
+  const [history, setHistory] = useState([]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleClick = (value) => {
+    if (value === "=") {
+      try {
+        // Evaluate the expression
+        const result = eval(displayValue);
+        setDisplayValue(result);
+        setHistory([...history, `${displayValue} = ${result}`]);
+      } catch (error) {
+        setDisplayValue("Error");
+      }
+    } else if (value === "C") {
+      // Clear the display
+      setDisplayValue("");
+    } else {
+      // Append the value to the display
+      setDisplayValue((prevValue) => prevValue + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <h1>Calculator</h1>
+      <div className="display">
+        <input type="text" value={displayValue} readOnly />
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={() => handleClick("/")}>/</button>
+
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button onClick={() => handleClick("*")}>*</button>
+
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("-")}>-</button>
+
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button onClick={() => handleClick("=")}>=</button>
+        <button onClick={() => handleClick("+")}>+</button>
+
+        <button onClick={() => handleClick("C")}>C</button>
+      </div>
+      <div className="history">
+        <h2>History</h2>
+        <ul>
+          {history.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
